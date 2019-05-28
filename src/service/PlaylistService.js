c896003bd7acdba82bcf6a8c56c0b8d8a3e55a4b
Jefferson F. Pires <jeffersfp@gmail.com>
2019-05-27 21:14:40 -0300

    Update PlaylistService to consume SpotifyService

--- src/service/PlaylistService.js
+++ src/service/PlaylistService.js
@@ -2,6 +2,21 @@
 
 
 const OpenWeatherService = require('./OpenWeatherService');
+const SpotifyService = require('./SpotifyService');
+
+/**
+ * 
+ **/
+function getMusicCategoryByWeather(weather) {
+    const temp = weather.main.temp;
+    if (temp < 10) {
+        return 'classical';
+    } else if (temp >= 10 && temp < 25) {
+        return 'rock';
+    } else {
+        return 'pop';
+    }
+}
 
 /**
  * Find songs that match the weather!
@@ -12,10 +27,12 @@ const OpenWeatherService = require('./OpenWeatherService');
 exports.findSongsByCityName = async function (city) {
     try {
         const weather = await OpenWeatherService.retrieveWeatherByCityName(city);
+        const musicCategory = getMusicCategoryByWeather(weather);
+        const playlist = await SpotifyService.getPlaylist(musicCategory, weather.sys.country);
         console.debug('PlaylistService call: success');
-        return weather;
+        return playlist;
     } catch (error) {
-        console.error('PlaylistService error: ', error);
-        return new Error(error);
+        console.error(`PlaylistService error: ${error}`);
+        throw new Error(error.message);
     }
 }
